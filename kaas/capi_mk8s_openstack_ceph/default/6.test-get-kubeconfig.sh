e614716e75cdf9c7817ebbea9d8d0f075189b3a2
dongmook <dongmook@acornsoft.io>
2023-06-08 16:43:33 +0900

    update multiple clusters

--- kaas/capi_mk8s_openstack_ceph/default/6.test-get-kubeconfig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./clusters -type f -name '*.yaml' -exec bash -c "basename {} .yaml | xargs -I %% sh -c '{ clusterctl --kubeconfig=$1 get kubeconfig %% > clusters_kubeconfig/%%; }'" \; 
