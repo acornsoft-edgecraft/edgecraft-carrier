503c23f7ac3514e76c68fd21b995277f074e1b8b
sunmi <u.flyingobject@gmail.com>
2023-10-05 14:19:16 +0900

    remove comment

--- 22apps/monitoring group/consul/00.helm_add_repo.sh
+++ 22apps/monitoring group/consul/00.helm_add_repo.sh
@@ -4,8 +4,8 @@ set -eEx
 VERSION="1.2.0"
 
 # add charts repo
-# helm repo add hashicorp https://helm.releases.hashicorp.com
-# helm repo update
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm repo update
 
 # download charts
 helm pull --untar -d ./assets hashicorp/consul --version ${VERSION}
